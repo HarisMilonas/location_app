f0370e9bfaa2fd059ccc6b61c930e240aa4fbdbe
Haris Mylonas <webdeveloper2@lalizas.com>
2024-04-19 12:51:58 +0300

    Test 2

--- lib/home.dart
+++ lib/home.dart
@@ -133,7 +133,7 @@ class _MyHomePageState extends State<MyHomePage> {
         mapController: mapController,
         options: MapOptions(
           interactionOptions:
-              const InteractionOptions(flags: InteractiveFlag.none),
+              const InteractionOptions(flags: InteractiveFlag.none,),
           initialCenter:
               LatLng(pickedLocation!.latitude, pickedLocation!.longitude),
           initialZoom: 13.0,
